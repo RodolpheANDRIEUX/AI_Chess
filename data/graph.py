62a8f28b5535ec9ea7cd8d54ed4a755549cf28bf
Rodolphe <rodolpheandrieux@gmail.com>
2024-07-02 03:48:34 +0200

    graph

--- data/graph.py
+++ data/graph.py
@@ -5,21 +5,21 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('../games.csv', sep=';')
 game_score = df.iloc[0]
-
+precision = 30
 print(game_score)
-x = np.linspace(1,  len(df.columns)-10, len(df.columns)-10)
+x = np.linspace(1,  len(df.columns)-precision, len(df.columns)-precision)
 y = np.zeros(len(x))
 z = np.zeros(len(x))
-for i in range(1, len(df.columns) - 10):
+for i in range(1, len(df.columns) - precision):
     wins = 0
     loses = 0
-    for j in range(10):
+    for j in range(precision):
         if game_score.iloc[i + j][0] == '1':
             wins += 1
         if game_score.iloc[i + j][4] == '1':
             loses += 1
-    y[i] = wins / 10 * 100
-    z[i] = 100 - (loses / 10 * 100)
+    y[i] = wins / precision * 100
+    z[i] = 100 - (loses / precision * 100)
 
 fig, ax = plt.subplots()
 ax.plot(x, y, color='green')
